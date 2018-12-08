496097a0304e7d05061dc09ee0834c18ebd51a76
natterstefan <stefan@natter.at>
2018-12-08 11:10:06 +0100

    add 100 demo videos to the playlist

--- App.js
+++ App.js
@@ -32,33 +32,21 @@ class PlaylistItem {
   }
 }
 
-const PLAYLIST = [
-  new PlaylistItem(
-    'Comfort Fit - “Sorry”',
-    'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Comfort_Fit_-_03_-_Sorry.mp3',
-    false,
-  ),
-  new PlaylistItem(
-    'Big Buck Bunny',
-    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-    true,
-  ),
-  new PlaylistItem(
-    'Mildred Bailey – “All Of Me”',
-    'https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3',
-    false,
-  ),
-  new PlaylistItem(
-    "Popeye - I don't scare",
-    'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4',
-    true,
-  ),
-  new PlaylistItem(
-    'Podington Bear - “Rubber Robot”',
-    'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3',
-    false,
-  ),
-];
+const PLAYLIST = [];
+
+/**
+ * DEMO VIDEO
+ * - any random url will currently resolve and provide the demo video .mp4 file
+ */
+for (let i = 0; i < 100; i++) {
+  PLAYLIST.push(
+    new PlaylistItem(
+      `Training Video ${i}`,
+      `https://test-rp.lukasender.at/foo-test-video-${i}`,
+      true,
+    ),
+  );
+}
 
 const ICON_THROUGH_EARPIECE = 'speaker-phone';
 const ICON_THROUGH_SPEAKER = 'speaker';
